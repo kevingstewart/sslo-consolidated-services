97a47a95d7132b9fa83545a06d8697edc0816e73
Kevin Stewart <kevin.g.stewart@gmail.com>
2021-01-13 11:57:45 -0500

    Create layer3-init.sh

---
+++ udf/configs/layer3-init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+apt update -y
+apt install apt-utils net-tools bridge-utils iproute2 -y
+
+sed -i 's/HOME_NET: \"\[192.168.0.0\/16,10.0.0.0\/8,172.16.0.0\/12\]\"/#HOME_NET: \"\[192.168.0.0\/16,10.0.0.0\/8,172.16.0.0\/12\]\"/g' /etc/suricata/suricata.yaml
+sed -i 's/#HOME_NET: \"any\"/HOME_NET: \"any\"/g' /etc/suricata/suricata.yaml
+sed -i 's/EXTERNAL_NET: \"!$HOME_NET\"/#EXTERNAL_NET: \"!$HOME_NET\"/g' /etc/suricata/suricata.yaml
+sed -i 's/#EXTERNAL_NET: \"any\"/EXTERNAL_NET: \"any\"/g' /etc/suricata/suricata.yaml
+sed -i 's/eth0/eth1/g' /etc/suricata/suricata.yaml
+sed -i 's/eth2/eth1/g' /etc/suricata/suricata.yaml
+sed -i 's/\/usr\/bin\/suricata \$\{ARGS\} \$\{SURICATA_OPTIONS\} \$@//g' /docker-entrypoint.sh
+
+ip route delete default
+ip route add default via 198.19.64.245
+ip route add 10.1.10.0/24 via 198.19.64.7
+
+/docker-entrypoint.sh
+/usr/bin/suricata -c /etc/suricata/suricata.yaml -s /etc/suricata/signatures.rules -i eth1
