3d0e44e1c355cfd39fdbb91aa3d467057035eec0
Kevin Stewart <kevin.g.stewart@gmail.com>
2023-11-01 14:47:20 -0400

    Update webserver-init-next.sh

--- esxi/configs/webserver-init-next.sh
+++ esxi/configs/webserver-init-next.sh
@@ -7,8 +7,9 @@ sed -ie 's/^#\(Include .*httpd-ssl.conf\)/\1/' /usr/local/apache2/conf/httpd.con
 sed -ie 's/^#\(LoadModule .*mod_ssl.so\)/\1/' /usr/local/apache2/conf/httpd.conf
 sed -ie 's/^#\(LoadModule .*mod_socache_shmcb.so\)/\1/' /usr/local/apache2/conf/httpd.conf
 
-ifconfig eth0:0 172.19.0.11/24
-ifconfig eth0:1 172.19.0.12/24
-ifconfig eth0:2 172.19.0.13/24
+device=$(ip a |egrep -o 'inet 192.168.100\..*' | cut -d' ' -f7)
+ifconfig ${device}:0 192.168.100.11/24
+ifconfig ${device}:1 192.168.100.12/24
+ifconfig ${device}:2 192.168.100.13/24
 
 /usr/local/bin/httpd-foreground
