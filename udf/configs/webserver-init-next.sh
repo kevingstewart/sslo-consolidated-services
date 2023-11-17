00fb57468352d2465a0e4a4bc3ee87d63eaf7a93
Kevin Stewart <kevin.g.stewart@gmail.com>
2023-11-17 15:09:30 -0500

    Create webserver-init-next.sh

---
+++ udf/configs/webserver-init-next.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+apt update -y
+apt install apt-utils net-tools iproute2 -y
+
+sed -ie 's/^#\(Include .*httpd-ssl.conf\)/\1/' /usr/local/apache2/conf/httpd.conf
+sed -ie 's/^#\(LoadModule .*mod_ssl.so\)/\1/' /usr/local/apache2/conf/httpd.conf
+sed -ie 's/^#\(LoadModule .*mod_socache_shmcb.so\)/\1/' /usr/local/apache2/conf/httpd.conf
+
+device=$(ip a |egrep -o 'inet 192.168.100\..*' | cut -d' ' -f7)
+ifconfig ${device}:0 192.168.100.11/24
+ifconfig ${device}:1 192.168.100.12/24
+ifconfig ${device}:2 192.168.100.13/24
+
+/usr/local/bin/httpd-foreground
