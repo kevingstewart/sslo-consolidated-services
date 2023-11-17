70704fb1536eff849eca95f2ac48d2df728e65bd
Kevin Stewart <kevin.g.stewart@gmail.com>
2023-11-17 15:03:47 -0500

    Create security-services-rebuild-next.sh

---
+++ esxi/security-services-rebuild-next.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Use this script to completely destroy and rebuild the docker-compose containers environment from scratch.
+## Step 1: import this file to the directly with docker-services.all.yaml file
+## Step 2: Make it executable: chmod +x security-services-rebuild.sh
+## Step 3: Execute: ./security-services-rebuild.sh
+
+
+docker container stop $(docker container ls -q)
+docker container rm -f $(docker container ls -aq)
+docker image rm -f $(docker image ls -q)
+docker system prune -f
+docker-compose -f docker-services-all-nextlab.yaml up -d
+docker ps
